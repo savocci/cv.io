b843a2201913e01d359afdd70c7c3060e854ae05
savocci <66638956+savocci@users.noreply.github.com>
2023-09-27 14:47:55 +0300

    Create resize.js

---
+++ resize.js
@@ -0,0 +1,15 @@
+var contentSection = document.getElementsByClass("content-section")[0];
+
+var updateMarginBottom = function () {
+  var contentHeight = contentSection.clientHeight;
+  var windowHeight = window.innerHeight;
+  var footerHeight = 60;
+  var minMargin = 20;
+
+  var remainingSpace = windowHeight - contentHeight - footerHeight;
+
+  contentSection.style.marginBottom = Math.max(remainingSpace, minMargin) + "px";
+};
+
+window.addEventListener("resize", updateMarginBottom);
+window.addEventListener("load", updateMarginBottom);
