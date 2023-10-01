1d1f8ca3c37fb3804cb77fe46406ca0cc333c8c2
savocci <66638956+savocci@users.noreply.github.com>
2023-10-01 22:22:05 +0300

    Update topnav.js

--- topnav.js
+++ topnav.js
@@ -1,20 +1,8 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  var links = x.getElementsByTagName("a");
-
   if (x.className === "topnav") {
     x.className += " responsive";
-
-    for (var i = 0; i < links.length; i++) {
-      if (!links[i].classList.contains("icon")) {
-        links[i].className += " responsive"
-      }
-    }
   } else {
     x.className = "topnav";
-
-    for (var i = 0; i < links.length; i++) {
-      links[i].className = "navlink"
-    }
   }
 }
